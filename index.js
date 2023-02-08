a9dd996bd208378a2e7b74f8238e8e1b8f7f8a85
mori <remi.irifargrimson@gmail.com>
2023-02-08 22:11:00 +0100

    authentification

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 console.log("Hello Script")
 
-// QuerySelectors =====================================
+// QuerySelectors ==============================================
+// User Creation
 createButton = document.querySelector("#createButton")
-
-apiMessage = document.querySelector("#apiMessage")
-
 UserNameField = document.querySelector("#userName")
 UserMailField = document.querySelector("#userMail")
 userPasswordField = document.querySelector("#userPassword")
+
+// User authentification
+authButton = document.querySelector("#authButton")
+UserNameFieldAuth = document.querySelector("#userNameAuth")
+UserMailFieldAuth = document.querySelector("#userMailAuth")
+userPasswordFieldAuth = document.querySelector("#userPasswordAuth")
+
+
+
+// Api text response display
+apiMessage = document.querySelector("#apiMessage")
 
 
 // Fonction d'affichage du message
@@ -18,7 +27,7 @@ apiMessage.innerText = message
 
 
 
-
+// Eventlistener CREATION
 createButton.addEventListener("click", ()=>{
 
     console.log("BUTTON CLICKED")
@@ -37,16 +46,43 @@ createButton.addEventListener("click", ()=>{
       method: 'POST', // or 'PUT'
       body: JSON.stringify(userData),
     })
+    // Turns the response into Json
     .then(res => res.json())
+
     .then((data) => {
 
       console.log(data)
       setApiMessage(data.message)
       
     })
+})
+
+// Eventlistener AUTHENTIFICATION
+authButton.addEventListener("click", ()=>{
+
+    console.log("BUTTON CLICKED")
 
 
+    // Sets inputs values into the userData Object
+    userData = {
+      "pseudo" : UserNameFieldAuth.value,
+      "password" : userPasswordFieldAuth.value
+    }
 
+    // Fetches the user/create URL
+    fetch('http://chaos_citadel.test/user/authenticate',
+    {
+      method: 'POST', // or 'PUT'
+      body: JSON.stringify(userData),
+    })
+    // Turns the response into Json
+    .then(res => res.json())
 
+    .then((data) => {
+
+      console.log(data)
+      setApiMessage(data.token)
+      
+    })
 })
 
