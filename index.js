e822c81d8d505be0119088ff0b2f33bb8d804d2c
mori <remi.irifargrimson@gmail.com>
2023-02-08 16:17:48 +0100

    Creation of character

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 console.log("Hello Script")
 
-
+// QuerySelectors =====================================
 createButton = document.querySelector("#createButton")
 
-userData = {
-    "pseudo" : "thename",
-    "mail" : "email@email.com",
-    "password" : "thepassword"
+apiMessage = document.querySelector("#apiMessage")
+
+UserNameField = document.querySelector("#userName")
+UserMailField = document.querySelector("#userMail")
+userPasswordField = document.querySelector("#userPassword")
+
+
+// Fonction d'affichage du message
+function setApiMessage(message){
+apiMessage.innerText = message
 }
 
-createButton.addEventListener("click", ()=>{
-    console.log("BUTTON CLICK")
 
-    fetch('http://chaos_citadel.test/user/create', {
-        mode: "no-cors",
-        method: 'POST', // or 'PUT'
-        body: JSON.stringify(userData),
-      })
-      .then((data) => {
-        console.log('Success:', data);
-      })
+
+
+
+createButton.addEventListener("click", ()=>{
+
+    console.log("BUTTON CLICKED")
+
+
+    // Sets inputs values into the userData Object
+    userData = {
+      // "pseudo" : UserNameField.value,
+      // "mail" : UserMailField.value,
+      // "password" : userPasswordField.value
+    }
+
+    // Fetches the user/create URL
+    fetch('http://chaos_citadel.test/user/create',
+    {
+      method: 'POST', // or 'PUT'
+      body: JSON.stringify(userData),
+    })
+    .then(res => res.json())
+    .then((data) => {
+
+      console.log(data)
+      setApiMessage(data.message)
+      
+    })
+
+
 
 
 })
